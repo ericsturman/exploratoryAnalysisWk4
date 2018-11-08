d17e9795a64e28ebf66edb843a566a23c96ca010
eric <eric.sturman@gmail.com>
2018-11-07 22:24:35 -0600

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+NEI <- readRDS("./exdata%2Fdata%2FNEI_data/Source_Classification_Code.rds")
+SCC <- readRDS("./exdata%2Fdata%2FNEI_data/summarySCC_PM25.rds")
+
+str(NEI)
+str(SCC)
+
+summary(NEI)
+summary(SCC)
+
