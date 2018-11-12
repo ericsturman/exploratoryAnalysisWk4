d78752cbf49dc33e6236d2a3b1bfa2d5364e7bad
eric <eric.sturman@gmail.com>
2018-11-11 20:02:29 -0600

    adding questions 4 & 5

---
+++ question4.R
@@ -0,0 +1,17 @@
+FEI <- readRDS("./exdata%2Fdata%2FNEI_data/Source_Classification_Code.rds")
+SCC <- readRDS("./exdata%2Fdata%2FNEI_data/summarySCC_PM25.rds")
+
+rows<-grep("Coal|coal",names(table(FEI$Short.Name)))
+codes<-FEI[rows,1]
+
+emmisionsList <- as.numeric(SCC$Emissions)
+yearList <- SCC$year
+sourceList <- SCC$SCC
+
+emmisions <- emmisionsList[sourceList %in% codes]
+year <- yearList[sourceList %in% codes]
+
+newData <- data.frame(emmisions, year)
+
+aggDat<- aggregate(newData[,1], by=list(newData[,2]), FUN=sum)
+
---
+++ question5.R
@@ -0,0 +1,18 @@
+FEI <- readRDS("./exdata%2Fdata%2FNEI_data/Source_Classification_Code.rds")
+SCC <- readRDS("./exdata%2Fdata%2FNEI_data/summarySCC_PM25.rds")
+
+rows<-grep("Motor|motor|vehicle|Vehicle",names(table(FEI$Short.Name)))
+codes<-FEI[rows,1]
+
+emmisionsList <- as.numeric(SCC$Emissions)[SCC$fips == "24510"]
+yearList <- SCC$year[SCC$fips == "24510"]
+sourceList <- SCC$SCC[SCC$fips == "24510"]
+
+
+
+emmisions <- emmisionsList[sourceList %in% codes]
+year <- yearList[sourceList %in% codes]
+
+newData <- data.frame(emmisions, year)
+
+aggDat<- aggregate(newData[,1], by=list(newData[,2]), FUN=sum)
